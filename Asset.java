d0ec9d6512c9172bc32ecc6b4f8f05910c609fb0
Yaraalfouzan <alfouzan.yara@gmail.com>
2024-02-20 20:00:24 +0300

    messy comit
    ff

--- Asset.java
+++ Asset.java
@@ -9,4 +9,13 @@ class Asset {
         this.expectedReturn = expectedReturn;
         this.riskLevel = riskLevel;
     }
+    public String getId(){
+        return id;
+    }
+    public double getExpectedReturn(){
+        return expectedReturn;
+    }
+    public double getRiskLevel(){
+        return riskLevel;
+    }
 }
